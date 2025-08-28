dee4c749e8aa39293ee5c9885619c149c576ff6b
Lakshya Gupta <rakhunbam1@gmail.com>
2025-08-29 00:11:12 +0530

    first commit

--- models/ann_v004.py
+++ models/ann_v004.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 dataset = pd.read_csv("ESI-3.csv", sep=',')
 dataset = dataset.dropna()
 
@@ -101,6 +101,47 @@ def predict(X, W, b):
 
 
 # Example of making a prediction for a sample input
-sample_input = X[8].reshape(1, -1)  # Sample input (make sure to reshape if it's a single sample)
+sample_input = X[8].reshape(1, -1)  
 predicted_value = predict(sample_input, W, b)
 print("Predicted value:", predicted_value[0], Y[8])
+
+# Multiple parameters optimisation algorithm
+
+input_file = "habitability_scored_data.csv"
+df = pd.read_csv(input_file,sep=';')
+
+p_esi_column = 'P_ESI'
+habitability_score_column = 'habitability_score'
+
+p_esi_data = df[p_esi_column].values
+habitability_score_data = df[habitability_score_column].values
+
+def compute_averages(data, chunk_size=50):
+    num_chunks = len(data) // chunk_size
+    averaged_values = []
+    for i in range(num_chunks):
+        chunk = data[i * chunk_size:(i + 1) * chunk_size]
+        averaged_values.append(np.mean(chunk))
+    return averaged_values
+
+averaged_p_esi = compute_averages(p_esi_data)
+averaged_habitability_score = compute_averages(habitability_score_data)
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 1, 1)
+plt.bar(range(len(averaged_p_esi)), averaged_p_esi, color='lightblue')
+plt.xlabel('Chunk Number')
+plt.ylabel('Average P_ESI')
+plt.title('Average P_ESI of Every 50 Data Points')
+plt.xticks(range(len(averaged_p_esi)), [f'Chunk {i+1}' for i in range(len(averaged_p_esi))])
+
+plt.subplot(2, 1, 2)
+plt.bar(range(len(averaged_habitability_score)), averaged_habitability_score, color='lightgreen')
+plt.xlabel('Chunk Number')
+plt.ylabel('Average Habitability Score')
+plt.title('Average Habitability Score of Every 50 Data Points')
+plt.xticks(range(len(averaged_habitability_score)), [f'Chunk {i+1}' for i in range(len(averaged_habitability_score))])
+
+plt.tight_layout()
+plt.show()
